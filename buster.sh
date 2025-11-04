d5a23c8e8666fe38474d618d99ae51fdb6c3dd65
neib <38737284+neib@users.noreply.github.com>
2025-11-04 15:18:43 +0100

    Update buster.sh

--- buster.sh
+++ buster.sh
@@ -6,15 +6,16 @@
 
 # Print help
 usage() {
-    echo "=== Web Buster +++ Web search tool for subdomains, directories, and files ==="
+    echo "---+++===[ Web Buster ]===+++---"
+    echo " A Web search tool for subdomains, directories, and files."
     echo
-    echo -e "\033[1mUsage:\033[0m $0 -m <mode> -u <URL> -w <wordlist> [--ignore-cert] [-z <milliseconds>] [--no-check] [[--no-slash]]"
+    echo -e "    \033[1mUsage:\033[0m $0 -m <mode> -u <URL> -w <wordlist> [--ignore-cert] [-z <milliseconds>] [--no-check] [[--no-slash]]"
     echo
     echo -e "\033[1mArguments:\033[0m"
     echo "  -h, --help                                  Print this help."
     echo "  -m <mode>, --mode <mode>                    Specify what to search for."
     echo "                                              The mode can be : sub (subdomain discovery),"
-    echo "                                                                dir(directory discovery),"
+    echo "                                                                dir (directory discovery),"
     echo "                                                                file (file discovery)."
     echo "  -u <url>, --url <url>                       Specify a target."
     echo "  -w <wordlist>, --wordlist <wordlist>        Specify a dictionnary to use."
@@ -23,6 +24,7 @@ usage() {
     echo "  --ignore-cert                               Perform 'insecure' SSL connection."
     echo "  -z <milliseconds>, --timer <milliseconds>   Waiting between requests."
     echo "  --no-check                                  Do not attempt to contact the site initially."
+    echo "  -v, --verbose                               Verbose mode."
     echo
     echo -e "\033[1mMode dir only:\033[0m"
     echo "  --no-slash                                  Do not add a final '/' to the directory name."
@@ -35,9 +37,11 @@ usage() {
     echo
 }
 
-# No argument, print help and exit
+# No argument, print Usage and exit
 if [[ $# -eq 0 ]]; then
-    usage
+    echo -e "---+++===[ Web Buster ]===+++---\n A Web search tool for subdomains, directories, and files.\n"
+    echo -e "\033[1mUsage:\033[0m $0 -m <mode> -u <URL> -w <wordlist> [--ignore-cert] [-z <milliseconds>] [[--no-slash]]\n"
+    echo -e "Type '$0 --help' for more information."
     exit 1
 fi
 
@@ -51,45 +55,42 @@ while [[ "$#" -gt 0 ]]; do
         --ignore-cert) NOCERT=1 ;;
         -z|--timer) TIMER=$2; shift ;;
         --no-check) NOCHECK=1 ;;
+        -v|--verbose) VERBOSE=1 ;;
         --no-slash) NOSLASH=1 ;;
-        #*) echo -e "\nError: Bad argument $1.\n"; usage; exit 1 ;;
-        *) echo -e "\nError: Bad argument $1.\n"; exit 1 ;;
+        *) echo -e "\nError : Bad argument $1.\n"; exit 1 ;;
     esac
     shift
 done
 
+# Title
+echo -e "---+++===[ Web Buster ]===+++---\n A Web search tool for subdomains, directories, and files.\n"
+
 # Required arguments
 if [[ -z "$MODE" ]]; then
-    echo -e "\nError: --mode is required.\n"
-    #usage
+    echo -e "\nError : --mode is required.\n"
     exit 1
 elif [[ -z "$URL" ]]; then
-    echo -e "\nError: --url is required.\n"
-    #usage
+    echo -e "\nError : --url is required.\n"
     exit 1
 elif [[ -z "$WORDS" ]]; then
-    echo -e "\nError: --wordlist is required.\n"
-    #usage
+    echo -e "\nError : --wordlist is required.\n"
     exit 1
 fi
 
 # Bad argument
 if [[ "$MODE" == "sub" ]]; then
     if [[ $NOSLASH == 1 ]]; then
-        echo -e "\nError: Bad argument --no-slash.\n"
-        #usage
+        echo -e "\nError : Bad argument --no-slash.\n"
         exit 1
     fi
 elif [[ "$MODE" == "file" ]]; then
     if [[ $NOSLASH == 1 ]]; then
-        echo -e "\nError: Bad argument --no-slash.\n"
-        #usage
+        echo -e "\nError : Bad argument --no-slash.\n"
         exit 1
     fi
 # Bad mode
 elif [[ "$MODE" != "dir" ]]; then
-    echo -e "\nError: Bad mode : $MODE.\n"
-    #usage
+    echo -e "\nError : Bad mode : $MODE.\n"
     exit 1
 fi
 
@@ -99,30 +100,28 @@ if [[ $URL =~ ^(https?://)([^/]+) ]]; then
     if [[ "$MODE" == "sub" ]]; then
         protocol=${BASH_REMATCH[1]}  # "https://"
         domain=${BASH_REMATCH[2]}    # "domain.com"
-        echo -e "\n[+] MODE : Subdomain Buster"
+        echo "[+] MODE : Subdomain Buster"
     else
         # The string does not end with a slash
         if [[ ! "$URL" =~ /$ ]]; then
             URL="$URL/"
         fi
         if [[ "$MODE" == "file" ]]; then
-            echo -e "\n[+] MODE : File Buster"
+            echo "[+] MODE : File Buster"
         elif [[ "$MODE" == "dir" ]]; then
-            echo -e "\n[+] MODE : Directory Buster"
+            echo "[+] MODE : Directory Buster"
         fi
     fi
     echo "[+] TARGET : $URL"
 else
-    echo -e "\nError: Bad URL format.\n"
-    #usage
+    echo -e "Error : Bad URL format.\n"
     exit 1
 fi
 
 # If a timer is defined, it must be an integer
 if [[ -v TIMER ]]; then
     if [[ ! "$TIMER" =~ ^[0-9]+$ ]]; then
-        echo -e "\nError: Time must be expressed in milliseconds.\n"
-        #usage
+        echo -e "\nError : Time must be expressed in milliseconds.\n"
         exit 1
     fi
 fi
@@ -140,13 +139,13 @@ if [[ $NOCHECK != 1 ]]; then
         CHECK_CODE=$(curl -s -o /dev/null -w "%{http_code}" $URL)
     fi
     if [[ "$CHECK_CODE" == "000" ]]; then
-        echo -e "\nError: The provided URL seems to be down.\n"
+        echo -e "\nError : The provided URL seems to be down.\n"
         exit 1
     fi
 fi
 
 # Add a blank line for clarity
-echo
+echo -e " Searching...\n"
 # Number of loot items
 DISCOVERED=0
 # Check the HTTP code and display discoveries
@@ -179,9 +178,9 @@ while read WHAT; do
 
     # Displays discoveries
     if [[ "$CODE" != "$CONTROL_CODE" ]]; then
-        echo "$TARGET [$CODE]"
+        echo "  $TARGET [$CODE]"
         ((DISCOVERED++))
-    else
+    elif [[ $VERBOSE == 1 ]]; then
         # Clear the last line if the HTTP code is not good
         echo -ne "\033[2K\r"
         echo -ne "$TARGET\r"
